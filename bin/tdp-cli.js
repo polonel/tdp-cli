add8b042beaffa5011be88954b7dc0774cf9b980
Chris Brame <polonel@gmail.com>
2017-03-09 22:30:12 -0500

    Fixed help menu

--- bin/tdp-cli.js
+++ bin/tdp-cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-;(function () { // wrapper in case we're in module_context mode
+;(function () { 
 
   process.title = 'tdp'
 
   var path = require('path')
   var tdp = require('../lib/tdp.js')
   var minimist = require('minimist');
+  var chalk = require('chalk');
   var argv = minimist(process.argv.slice(2));
 
   switch (argv._[0]) {
@@ -28,7 +29,16 @@
         tdp.remove(argv._[1]);
         break;
       default:
-        console.log('Invalid Command. Show Help here.');
+        console.log(chalk.red('Invalid Command.'));
+        console.log('');
+        console.log('tdp usage: tdp <command>');
+        console.log('where <command> is one of:');
+        console.log('   config             set the configuration for tdp');
+        console.log('   init               init a new trudesk plugin in the given folder');
+        console.log('   view <plugin>      search a view details of the given plugin');
+        console.log('   register           register and upload the plugin to the central plugin repository')
+        console.log('   install <plugin>   download and install the plugin to the configured trudesk server');
+        console.log('   remove <plugin>    remove the given plugin from the configured trudesk server');
   }
 
 //   log.verbose('cli', process.argv)
